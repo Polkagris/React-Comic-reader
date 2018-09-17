8f242fc79494d3a7f900e2bfa183e34f2a4998ba
Arny <arne.n.bjerkaas@gmail.com>
2018-09-17 13:52:44 +0200

    Able to change default image on click

--- src/Components-stateless/Default-image.js
+++ src/Components-stateless/Default-image.js
@@ -4,14 +4,30 @@ import MyComponent from '../Mycomp.js'
 
 // Pass state.url to image src
 
-const Defaultimage = (props) =>{
-  return(
-    <div className="row">
-      <h2>ChildComponent name: {props.name} url: {props.url}!</h2>
-      <img id="defaultImage" className="col-lg-8" src={props.url}>
-      </img>
-    </div>
-  );
+class Defaultimage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      childUrl: 'https://images.unsplash.com/photo-1519498955853-621f66b86038?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c061931e7820a66c483fc8f6a6778bbe&auto=format&fit=crop&w=1352&q=80'
+    }
+    this.nextImage = this.nextImage.bind(this);
+  }
+  // Change default image
+  nextImage(){
+  console.log("test complete");
+  // Change url state
+  this.setState({childUrl: "https://images.unsplash.com/photo-1523480429206-eb448003a1c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d9bd24be13facae53061d2b794b95ddf&auto=format&fit=crop&w=1350&q=80"});
+  }
+  render(){
+    return(
+      <div className="row">
+        <h2>ChildComponent url: {this.state.childUrl}!</h2>
+        <img id="defaultImage" className="col-lg-8" src={this.state.childUrl} onClick={this.nextImage}>
+        </img>
+      </div>
+    );
+  }
+
 }
 
 
--- src/Mycomp.js
+++ src/Mycomp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Defaultimage from './Components-stateless/Default-image.js';
 
 class MyComponent extends React.Component {
   constructor(props) {
@@ -9,7 +10,6 @@ class MyComponent extends React.Component {
       url: 'https://images.unsplash.com/photo-1519498955853-621f66b86038?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c061931e7820a66c483fc8f6a6778bbe&auto=format&fit=crop&w=1352&q=80'
     };
     this.handleClick = this.handleClick.bind(this);
-    this.nextImage = this.nextImage.bind(this);
   }
   // Toggle text
   handleClick() {
@@ -24,16 +24,12 @@ class MyComponent extends React.Component {
     }
   }
 
-  // Change default image
-  nextImage(){
-  console.log("test complete");
-  // Change url state
-  this.setState({url: "https://images.unsplash.com/photo-1523480429206-eb448003a1c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d9bd24be13facae53061d2b794b95ddf&auto=format&fit=crop&w=1350&q=80"});
-  }
+
 
   render() {
     return (
       <div>
+        <Defaultimage/>
         <button onClick={this.handleClick}>Click Me</button>
         <h1>{this.state.name}</h1>
         <button onClick={this.nextImage}>Click For Next Image</button>
