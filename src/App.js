d6c850c4a8643199438f7c8652387f4361c8667a
Arny <arne.n.bjerkaas@gmail.com>
2018-09-19 10:02:01 +0200

    Able to change default image to the the next thumbnails on click

--- src/App.js
+++ src/App.js
@@ -12,16 +12,10 @@ import MyComponent from './Mycomp.js'
 class App extends Component {
   render() {
 
-    // Change the default image
-      function nextDefaultImg(){
-        console.log("123");
-      };
     return (
       <div className="App">
         <div className="container">
-
           <h1 id="title">Comic Reader</h1>
-          <Defaultimage onClick={this.nextDefaultImg} name="Christopher" url="https://images.unsplash.com/photo-1519498955853-621f66b86038?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c061931e7820a66c483fc8f6a6778bbe&auto=format&fit=crop&w=1352&q=80" />
           <MyComponent/>
           <Thumbnails/>
 
--- src/Components-stateless/Default-image.js
+++ src/Components-stateless/Default-image.js
@@ -1,29 +1,93 @@
 import React, { Component } from 'react';
 import '../App.css';
-import MyComponent from '../Mycomp.js'
+import MyComponent from '../Mycomp.js';
+import Thumbnails from './Thumbnails.js';
 
 // Pass state.url to image src
+
+
 
 class Defaultimage extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      childUrl: 'https://images.unsplash.com/photo-1519498955853-621f66b86038?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c061931e7820a66c483fc8f6a6778bbe&auto=format&fit=crop&w=1352&q=80'
+      childUrl: 'https://images.unsplash.com/photo-1519498955853-621f66b86038?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c061931e7820a66c483fc8f6a6778bbe&auto=format&fit=crop&w=1352&q=80',
+      thumb1: 'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a53f6ccb2fccd80ae7f6c6af8f289319&auto=format&fit=crop&w=1350&q=80',
+      thumb2: 'https://images.unsplash.com/photo-1523480429206-eb448003a1c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d9bd24be13facae53061d2b794b95ddf&auto=format&fit=crop&w=1350&q=80',
+      thumb3: 'https://images.unsplash.com/photo-1526243741027-444d633d7365?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7491eb281478f3b97661bae38cbcb34b&auto=format&fit=crop&w=1951&q=80',
+      thumb4: 'https://images.unsplash.com/photo-1517770413964-df8ca61194a6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8a2a753a335178a63b40af45ed76f78f&auto=format&fit=crop&w=1350&q=80'
     }
     this.nextImage = this.nextImage.bind(this);
+    this.clickedThumbnail = this.clickedThumbnail.bind(this);
   }
-  // Change default image
+  // Change default image onClick
   nextImage(){
-  console.log("test complete");
-  // Change url state
-  this.setState({childUrl: "https://images.unsplash.com/photo-1523480429206-eb448003a1c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d9bd24be13facae53061d2b794b95ddf&auto=format&fit=crop&w=1350&q=80"});
+    if(this.state.childUrl == 'https://images.unsplash.com/photo-1519498955853-621f66b86038?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c061931e7820a66c483fc8f6a6778bbe&auto=format&fit=crop&w=1352&q=80'){
+      this.setState({
+        childUrl: this.state.thumb1
+      });
+    }
+    else if(this.state.childUrl == this.state.thumb1){
+      this.setState({
+        childUrl: this.state.thumb2
+      });
+    }
+    else if(this.state.childUrl == this.state.thumb2){
+      this.setState({
+        childUrl: this.state.thumb3
+      });
+    }
+    else if(this.state.childUrl == this.state.thumb3){
+      this.setState({
+        childUrl: this.state.thumb4
+      });
+    }
+    else if(this.state.childUrl == this.state.thumb4){
+      this.setState({
+        childUrl: 'https://images.unsplash.com/photo-1519498955853-621f66b86038?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c061931e7820a66c483fc8f6a6778bbe&auto=format&fit=crop&w=1352&q=80'
+      });
+    }
   }
+
+  clickedThumbnail(event){
+          console.log("Clicked successfully!");
+    this.setState({
+      childUrl: this.src
+    });
+  }
+
+
   render(){
+    let defaultImage = ['https://images.unsplash.com/photo-1517770413964-df8ca61194a6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8a2a753a335178a63b40af45ed76f78f&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1526243741027-444d633d7365?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7491eb281478f3b97661bae38cbcb34b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1523480429206-eb448003a1c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d9bd24be13facae53061d2b794b95ddf&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a53f6ccb2fccd80ae7f6c6af8f289319&auto=format&fit=crop&w=1350&q=80'];
+
+
     return(
       <div className="row">
-        <h2>ChildComponent url: {this.state.childUrl}!</h2>
         <img id="defaultImage" className="col-lg-8" src={this.state.childUrl} onClick={this.nextImage}>
         </img>
+        <ThumbnailsTest onClick={this.clickedThumbnail}/>
+      </div>
+    );
+  }
+}
+
+class ThumbnailsTest extends React.Component{
+  constructor(props){
+    super(props);
+
+      //this.clickedThumbnail = this.clickedThumbnail.bind(this);
+  }
+
+  render(){
+    return(
+      <div id="thumbnails">
+        <img onClick={this.clickedThumbnail} id="thumb1" src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a53f6ccb2fccd80ae7f6c6af8f289319&auto=format&fit=crop&w=1350&q=80" alt=""></img>
+        <img onClick={this.clickedThumbnail} id="thumb2" src="https://images.unsplash.com/photo-1523480429206-eb448003a1c5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d9bd24be13facae53061d2b794b95ddf&auto=format&fit=crop&w=1350&q=80" alt=""></img>
+        <img onClick={this.clickedThumbnail} id="thumb3" src="https://images.unsplash.com/photo-1526243741027-444d633d7365?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7491eb281478f3b97661bae38cbcb34b&auto=format&fit=crop&w=1951&q=80" alt=""></img>
+        <img onClick={this.clickedThumbnail} id="thumb4" src="https://images.unsplash.com/photo-1517770413964-df8ca61194a6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8a2a753a335178a63b40af45ed76f78f&auto=format&fit=crop&w=1350&q=80" alt=""></img>
       </div>
     );
   }
